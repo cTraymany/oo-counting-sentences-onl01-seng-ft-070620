be82f65aa693dfb97ac03605c4bb6048dbceec14
Chindalath Traymany <chindalatht@gmail.com>
2020-07-11 16:51:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    sentences_array = self.split(".+", "!+", "?+")
+    sentences_array = self.split(".*", "!*", "?*")
     if sentences_array.empty?
       0
     else
